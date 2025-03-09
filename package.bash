d4653802b7842af55e6986346578c2bac8f94cbd
Edmilson Monteiro Rodrigues Neto <edmilson.rodriguesn38@gmail.com>
2025-03-09 15:43:29 +0000

    feat: add repo.proto to start creating the gRPC

--- package.bash
+++ package.bash
@@ -2,4 +2,4 @@ VERSION="1.0.0"
 sed -i "s/Version: .*/Version: $VERSION/" deb-packaging/DEBIAN/control
 sed -i "s/version: .*/version: $VERSION/" snap/snapcraft.yaml
 dpkg-deb --build deb-packaging "dist/ophelia-ci-server_${VERSION}_amd64.deb"
-snapcraft
+sudo snapcraft
